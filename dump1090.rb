94859a91e99a8daa312898295b58abfb3a36a626
Dustin Brewer <mannkind@thenullpointer.net>
2014-12-15 22:49:34 -0800

    Initial Formula Commit

---
+++ dump1090.rb
@@ -0,0 +1,33 @@
+require "formula"
+
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Dump1090 < Formula
+  homepage "https://github.com/MalcolmRobb/dump1090"
+  head 'git://github.com/MalcolmRobb/dump1090.git'
+
+  env :std
+
+  depends_on "robotastic/hackrf/rtlsdr"
+
+  # depends_on "cmake" => :build
+  #depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # Compile
+    system "make"
+
+    # Create a hacky shell script to cd into the working dir, then pass any dump1090 params
+    system "echo \"cd #{prefix} && ./dump1090 \\$@\" > #{prefix}/dump1090.sh"
+    system "chmod +x #{prefix}/dump1090.sh"
+    
+    # Manually install
+    system "install -m 755 dump1090 #{prefix}"
+    system "cp -r public_html #{prefix}"
+
+    # Manually create the symlink
+    system "ln -fs #{prefix}/dump1090.sh /usr/local/bin/dump1090"
+  end
+end
