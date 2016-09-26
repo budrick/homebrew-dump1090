d5b702f6b3cf28e80a0a019f86b1d746d539df98
Matt Cegielka <matt@mudbrick.org>
2016-09-26 22:24:37 +0100

    Add dump1090-mutability and depends_on libusb

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # Dump1090 Homebrew Tap
 
+Includes formulae for [MalcomRobb](https://github.com/MalcolmRobb/dump1090) and [mutability](https://github.com/MalcolmRobb/dump1090) variations.
+    
 ## Install
+
 ```
-brew tap mannkind/dump1090
+brew tap budrick/dump1090
+```
+
+Then:
+
+```
 brew install dump1090 --HEAD
 ```
+
+Or:
+
+```
+brew install dump1090-mutability --HEAD
+```
+
--- dump1090-mutability.rb
+++ dump1090-mutability.rb
@@ -4,9 +4,9 @@ require "formula"
 #                /usr/local/Library/Contributions/example-formula.rb
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
-class Dump1090 < Formula
-  homepage "https://github.com/MalcolmRobb/dump1090"
-  head 'git://github.com/MalcolmRobb/dump1090.git'
+class Dump1090Mutability < Formula
+  homepage "https://github.com/mutability/dump1090"
+  head 'git://github.com/mutability/dump1090.git'
 
   env :std
 
@@ -23,7 +23,7 @@ class Dump1090 < Formula
     # Create a hacky shell script to cd into the working dir, then pass any dump1090 params
     system "echo \"cd #{prefix} && ./dump1090 \\$@\" > #{prefix}/dump1090.sh"
     system "chmod +x #{prefix}/dump1090.sh"
-    
+
     # Manually install
     system "install -m 755 dump1090 #{prefix}"
     system "cp -r public_html #{prefix}"
