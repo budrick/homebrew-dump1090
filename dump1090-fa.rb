0927b913ff4e24876a0f94e6fb3d44adc1f65cb6
Matt Cegielka <mattcegielka@aka.nyc>
2019-04-12 17:03:40 -0400

    Update dump1090-fa to build in the sandbox and with compat patches

--- dump1090-fa.rb
+++ dump1090-fa.rb
@@ -17,19 +17,28 @@ class Dump1090Fa < Formula
   # depends_on "cmake" => :build
   #depends_on :x11 # if your formula requires any X11/XQuartz components
 
+  head do
+    # FA fork of dump1090-mutability removes MacOS compatibility options from the Makefile. This restores them.
+    patch do
+      url "https://raw.githubusercontent.com/budrick/homebrew-dump1090/master/patches/dump1090-compat.patch"
+      sha256 "ad9752f153ea47917d136cbbb9c4200f00b3b82d3be3d145f8400d25c99e001e"
+    end
+  end
+
   def install
     # Compile
-    system "make"
+    system "make BLADERF=no"
 
     # Create a hacky shell script to cd into the working dir, then pass any dump1090 params
-    system "echo \"cd #{prefix} && ./dump1090 \\$@\" > #{prefix}/dump1090.sh"
-    system "chmod +x #{prefix}/dump1090.sh"
+    system "echo \"cd #{prefix} && ./dump1090 \\$@\" > #{prefix}/dump1090-fa"
+    system "chmod +x #{prefix}/dump1090-fa"
 
     # Manually install
     system "install -m 755 dump1090 #{prefix}"
     system "cp -r public_html #{prefix}"
 
     # Manually create the symlink
-    system "ln -fs #{prefix}/dump1090.sh /usr/local/bin/dump1090-fa"
+    # system "ln -fs #{prefix}/dump1090.sh /usr/local/bin/dump1090-fa"
+    bin.install ["#{prefix}/dump1090-fa"]
   end
 end
