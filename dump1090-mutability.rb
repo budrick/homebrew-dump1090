ffbcf7496fcacc00ce1402b066fa12393d9c0664
Matt Cegielka <mattcegielka@aka.nyc>
2019-04-12 17:03:17 -0400

    Update dump1090-mutability to build in the sandbox

--- dump1090-mutability.rb
+++ dump1090-mutability.rb
@@ -21,14 +21,16 @@ class Dump1090Mutability < Formula
     system "make"
 
     # Create a hacky shell script to cd into the working dir, then pass any dump1090 params
-    system "echo \"cd #{prefix} && ./dump1090 \\$@\" > #{prefix}/dump1090.sh"
-    system "chmod +x #{prefix}/dump1090.sh"
+    system "echo \"cd #{prefix} && ./dump1090 \\$@\" > #{prefix}/dump1090-mutability"
+    system "chmod +x #{prefix}/dump1090-mutability"
 
     # Manually install
     system "install -m 755 dump1090 #{prefix}"
     system "cp -r public_html #{prefix}"
 
     # Manually create the symlink
-    system "ln -fs #{prefix}/dump1090.sh /usr/local/bin/dump1090-mutability"
+    # system "ln -fs #{prefix}/dump1090-mutability /usr/local/bin/dump1090-mutability"
+    bin.install ["#{prefix}/dump1090-mutability"]
+
   end
 end
